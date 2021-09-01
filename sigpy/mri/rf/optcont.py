0fef5dbf34c4bf2d1accdc31542b4a6bfce90832
Jonathan Martin <44706383+jonbmartin@users.noreply.github.com>
2021-08-31 20:34:40 -0500

    streamlined optimal control designer

--- sigpy/mri/rf/optcont.py
+++ sigpy/mri/rf/optcont.py
@@ -2,57 +2,32 @@
 """Optimal Control Pulse Design functions.
 """
 from sigpy import backend
-from sigpy.mri.rf import slr
-from sigpy.mri.rf import util
+import sigpy as sp
 import numpy as np
 import jax as jax
-import jax.numpy as jnp
-from jax import jit
-import time
 
-__all__ = ['rf_autodiff', 'optcont1d', 'blochsim', 'deriv']
+__all__ = ['rf_autodiff', 'mxy_loss', 'optcont1d', 'blochsim', 'deriv']
 
 
 def rf_autodiff(rfp, b1, mxd, myd, mzd, w, niters=5, step=0.00001, mx0=0, my0=0, mz0=1.0, ):
-    # err_jit = jit(util.bloch_sim_err)
-    # err_grad = jax.grad(err_jit)
-    # err_grad = jax.grad(util.bloch_sim_err_combined)
-    # err_grad = jit(err_grad)
-    err_jac = jax.jacfwd(util.bloch_sim_err_single)
-    print('Finish autodiff')
 
-    rfp_abs = jnp.absolute(rfp)
-    rfp_angle = jnp.angle(rfp)
-    rf_op = jnp.append(rfp_abs, rfp_angle)
-    N = len(rf_op)
-    nt = jnp.floor(N / 2).astype(int)
-    print('Start iters')
-    t0 = time.time()
-
-    # for nn in range(niters):
-    #     J = err_grad(rf_op, b1, mx0, my0, mz0, nt, mxd, myd, mzd, w)
-    #     rf_op -= step * J
-    #     print('Finish iter. Time: {:f}'.format(time.time()-t0))
-
-    # for nn in range(niters):
-    #     J = np.zeros(N)
-    #     for ii in range(b1.size):
-    #         J += err_grad(rf_op, b1[ii], mx0, my0, mz0, nt, mxd[ii], myd[ii], mzd[ii],
-    #                       w[ii])
-    #         print('Finish one b1. Time: {:f}'.format(time.time() - t0))
-    #     rf_op -= step * J
-    #     print('Finish iter. Time: {:f}'.format(time.time()-t0))
+    err_jac = jax.jacfwd(mxy_loss)
+    N = len(rfp)
+    nt = np.floor(N / 2).astype(int)
 
     for nn in range(niters):
         J = np.zeros(N)
+        mx, my, mz = sp.mri.rf.arb_b1_phase_sim(rfp, b1, mx0, my0, mz0, nt)
         for ii in range(b1.size):
-            J += err_jac(jnp.array([rf_op, b1[ii], mx0, my0, mz0, nt, mxd[ii], myd[ii], mzd[ii],
-                                    w[ii]]))
-            print('Finish one b1. Time: {:f}'.format(time.time() - t0))
-        rf_op -= step * J
-        print('Finish iter. Time: {:f}'.format(time.time() - t0))
+            J += err_jac(mx[ii], my[ii], mz[ii], mxd[ii], myd[ii], mzd[ii],
+                          w[ii])
+        rfp -= step * np.array(J)
 
-    return rf_op
+    return rfp
+
+
+def mxy_loss(mx, my, mz, mxd, myd, mzd, w):
+    return w * ((mx - mxd) ** 2 + (my - myd) ** 2 + (mz - mzd) ** 2)
 
 
 def optcont1d(dthick, N, os, tb, stepsize=0.001, max_iters=1000, d1=0.01,
@@ -88,7 +63,7 @@ def optcont1d(dthick, N, os, tb, stepsize=0.001, max_iters=1000, d1=0.01,
     # set up target beta pattern
     d1 = np.sqrt(d1 / 2)  # Mxy -> beta ripple for ex pulse
     d2 = d2 / np.sqrt(2)
-    dib = slr.dinf(d1, d2)
+    dib = sp.mri.rf.slr.dinf(d1, d2)
     ftwb = dib / tb
     # freq edges, normalized to 2*nyquist
     fb = np.asarray([0, (1 - ftwb) * (tb / 2),
